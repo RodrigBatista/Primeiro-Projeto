6e520212d7616814b3109de18efd9bad9f4e8c31
Rodrigo Batista <rodrigosantos8745@hotmail.com>
2024-04-11 23:03:48 -0300

    Testando JS

--- Scripts/Script16.js
+++ Scripts/Script16.js
@@ -15,10 +15,11 @@ for(var pos = 0; pos < valores.length; pos++) {
 
 
 var valores = [8, 1, 7, 4, 2, 9]
+valores.push(3, 8, 4, 12) // Coloca o valor '3, 8, 4, 12' nos últimos números
 for (let pos in valores) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
-var pos = valores.indexOf(9) // procurar o valor '9' dentro da var 'valores'
+var pos = valores.indexOf(7) // procurar o valor '7' dentro da var 'valores'
 if (pos == -1) {
     console.log('O valor não foi encontrado!')
 } else {
