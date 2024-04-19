e8dcd92f27ae142e6e204e28c7b6afc543f61d04
Rodrigo Batista <rodrigosantos8745@hotmail.com>
2024-04-18 22:16:20 -0300

    Estudando JQuery
    Primeiro estudo sobre JQuery

---
+++ JQuery/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function (){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        console.log("submit");
+        e.preventDefault();
+    })
+})
