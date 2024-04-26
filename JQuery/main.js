3d7ceef5322ee796a25440623efe4151cba7a1cf
Rodrigo Batista <rodrigosantos8745@hotmail.com>
2024-04-26 09:39:30 -0300

    Finalizando JQuery
    Resolvendo bugs

--- JQuery/main.js
+++ JQuery/main.js
@@ -3,18 +3,18 @@ $(document).ready(function(){
         $('form').slideDown();
     })
 
-    $('#botao-cancelar').click(function (){
+    $('#botao-cancelar').click(function() {
         $('form').slideUp();
     })
 
     $('form').on('submit', function(e) {
         e.preventDefault();
-        const enderoDaNovaImagem = $('#endereco-imagem-nova').val();
-        const novoItem =$('<li style="display: none"></li>');
-        $(`<img src="${enderoDaNovaImagem}" />`).appendTo(novoItem);
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
         $(`
             <div class="overlay-imagem-link">
-                <a href="${enderoDaNovaImagem}" target="_blank" tittle="Ver imagem em tamanho real">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
                     Ver imagem em tamanho real
                 </a>
             </div>
